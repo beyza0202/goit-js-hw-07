348e4492a4eac37b33fd7904324db49f5d2a28aa
Beyza Karakehaya <beyza.karakehaya@gmail.com>
2026-03-12 04:56:20 +0300

    finish hw-07 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ const items = categories.querySelectorAll(".item");
 
 console.log(`Number of categories: ${items.length}`);
 
-items.forEach(item => {
+items.forEach((item) => {
   const title = item.querySelector("h2").textContent;
- const elements = item.querySelectorAll("li").length;
+  const elements = item.querySelectorAll("li").length;
 
   console.log(`Category: ${title}`);
   console.log(`Elements: ${elements}`);
--- js/task-2.js
+++ js/task-2.js
@@ -22,17 +22,17 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
 
 const gallery = document.querySelector(".gallery");
 
 const galleryItems = images
   .map(
-    image =>
+    (image) =>
       `<li class="gallery-item">
         <img src="${image.url}" alt="${image.alt}" />
-      </li>`
+      </li>`,
   )
   .join("");
 
