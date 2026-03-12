fba058a681c27b4291d8dde13ab7a00596c7d9a9
Beyza Karakehaya <beyza.karakehaya@gmail.com>
2026-03-12 04:54:38 +0300

    add task 6

---
+++ js/task-6.js
@@ -0,0 +1,46 @@
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100) return;
+
+  destroyBoxes();
+  createBoxes(amount);
+
+  input.value = "";
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 30;
+  const elements = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    elements.push(box);
+
+    size += 10;
+  }
+
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
